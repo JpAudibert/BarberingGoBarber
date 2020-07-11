92ddf698d47dc5e66d53582c1313bc890f9a193d
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-11 00:11:17 -0300

    database connection and functioning done

--- backend/src/database/migrations/1594427811513-CreateAppointments.ts
+++ backend/src/database/migrations/1594427811513-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1594427811513 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
