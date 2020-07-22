64936ae1f79fe476c3d199f281570158036b8ae0
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-21 23:41:21 -0300

    login page: visual. done

---
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+import logoSvg from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoSvg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <input placeholder="E-mail" />
+
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn size={16} />
+        Criar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default SignIn;
