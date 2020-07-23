2e40905c7834e2ff45dc8bf058aa8a8ff551caba
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-23 03:27:01 -0300

    toast hook, and toast component

--- gobarber-web/src/components/ToastContainer/index.tsx
+++ gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,42 +1,21 @@
 import React from 'react';
-import { FiAlertTriangle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import Toast from './Toast';
 
-const ToastContainer: React.FC = () => {
+import { ToastMessage } from '../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
-      <Toast hasDescription>
-        <FiAlertTriangle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Nao foi possivel fazer login na aplicacao</p>
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
-
-      <Toast hasDescription={false} type="success">
-        <FiAlertTriangle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
-
-      <Toast hasDescription type="error">
-        <FiAlertTriangle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Nao foi possivel fazer login na aplicacao</p>
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} toast={message} />
+      ))}
     </Container>
   );
 };
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoSvg from '../../assets/logo.svg';
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData): Promise<void> => {
@@ -39,7 +41,7 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -50,10 +52,14 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        
+        addToast({
+          type: 'error',
+          title: 'Error na autenticacao',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+        });
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
