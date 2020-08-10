5eae6a5690d44f19196ec2daf5de8c2615926853
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-08-10 15:17:22 -0300

    console logs removed

--- backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -22,15 +22,11 @@ export default class EtherealMailProvider implements IMailProvider {
   }
 
   public async sendMail(to: string, body: string): Promise<void> {
-    const message = await this.client.sendMail({
+    await this.client.sendMail({
       from: 'Equipe GoBarber <equipe@gobarber.com>',
       to,
       subject: 'Recuperacao de senha',
       text: body,
     });
-
-    console.log('Message sent: %s', message.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
