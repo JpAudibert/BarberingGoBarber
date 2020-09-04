cc5c6ec5c1704b73af6088da2bf56aa5026af9ff
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-09-04 17:23:43 -0300

    dashboard and day picker

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,143 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiPower, FiClock } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBraber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button onClick={signOut} type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horarios Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://pbs.twimg.com/profile_images/1293678150388461572/aPzH-u-n_400x400.jpg"
+                alt="Joao Pedro"
+              />
+
+              <strong>Joao Pedro Basso Audibert</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+              <div>
+                <img
+                  src="https://pbs.twimg.com/profile_images/1293678150388461572/aPzH-u-n_400x400.jpg"
+                  alt="Joao Pedro"
+                />
+
+                <strong>Joao Pedro Basso Audibert</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+              <div>
+                <img
+                  src="https://pbs.twimg.com/profile_images/1293678150388461572/aPzH-u-n_400x400.jpg"
+                  alt="Joao Pedro"
+                />
+
+                <strong>Joao Pedro Basso Audibert</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
--- gobarber-web/src/pages/Dashboard/styles.ts
+++ gobarber-web/src/pages/Dashboard/styles.ts
@@ -1,1 +1,285 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  padding: 32px 0;
+  background: #28262e;
+`;
+
+const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 80px;
+  }
+
+  button {
+    margin-left: auto;
+    background: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+      width: 20px;
+      height: 20px;
+    }
+  }
+`;
+
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 80px;
+
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      color: #999591;
+    }
+
+    strong {
+      color: #ff9000;
+    }
+  }
+`;
+
+const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+const Schedule = styled.section`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    color: #ff9000;
+    margin-top: 8px;
+    font-weight: 500;
+
+    span + span {
+      margin-left: 8px;
+      padding-left: 8px;
+      border-left: 1px solid #ff9000;
+    }
+  }
+`;
+
+const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      content: '';
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+    }
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+`;
+
+const Section = styled.section`
+  margin-top: 48px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  color: #f4ede8;
+
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    color: #ff9000;
+    margin-right: 8px;
+  }
+
+  div {
+    background: #3e3b47;
+    border-radius: 10px;
+    margin-left: 26px;
+    width: 100%;
+    height: 88px;
+    padding: 16px;
+
+    display: flex;
+    align-items: center;
+  }
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  img {
+    height: 56px;
+    width: 56px;
+    border-radius: 50%;
+    margin-right: 16px;
+  }
+`;
+
+const Calendar = styled.aside`
+  width: 380px;
+
+  .DayPicker {
+    border-radius: 10px;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+    background: #3e3b47;
+    border-radius: 10px;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-NavButton {
+    color: #999591 !important;
+  }
+
+  .DayPicker-NavButton--prev {
+    right: auto;
+    left: 1.5em;
+    margin-right: 0;
+  }
+
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px 0 0 0;
+    padding: 16px;
+    background-color: #28262e;
+    border-radius: 0 0 10px 10px;
+  }
+
+  .DayPicker-Caption {
+    margin-bottom: 1em;
+    padding: 0 1em;
+    color: #f4ede8;
+
+    > div {
+      text-align: center;
+    }
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: #3e3b47;
+    border-radius: 10px;
+    color: #fff;
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, '#3e3b47')};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+
+  .DayPicker-Day--disabled {
+    color: #666360 !important;
+    background: transparent !important;
+  }
+
+  .DayPicker-Day--selected {
+    background: #ff9000 !important;
+    border-radius: 10px;
+    color: #232129 !important;
+  }
+`;
+
+export {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+};
